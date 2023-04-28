f0a6ec95ee729bac332e185b1d45b2df22eeb323
taylor-green <taylor.green2804@gmail.com>
2023-04-28 14:03:59 -0400

    defined columns in product.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // id column 
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // product_name 
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // price 
+    price: {
+      type: DataTypes.DECIMAL, 
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    // category_id 
+    category_id: {
+      type: DataTypes.INTEGER,
+      // references the category model's id 
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
