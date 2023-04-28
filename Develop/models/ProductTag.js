604973ec5e6f713fb46ca2e2bc32355498e69220
taylor-green <taylor.green2804@gmail.com>
2023-04-28 14:59:21 -0400

    added tags to productTags.js

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,31 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // id 
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // product_id 
+    product_id: {
+      type: DataTypes.INTEGER,
+      // references the product model's id 
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    // tag_id 
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // references the tag model's id 
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
