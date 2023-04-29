1336ec7dbc63a6e7835068fface8259ce291fe97
taylor-green <taylor.green2804@gmail.com>
2023-04-29 15:13:15 -0400

    created post route to create new category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -24,10 +24,40 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'category_id']
+      }
+    ]
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'ERROR: Please check id'}); 
+        return; 
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
